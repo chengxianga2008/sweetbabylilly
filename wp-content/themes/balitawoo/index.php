cb6a49f3565cb4b4db04a59aa1de2846fc930b47
Jack Carrick Xiang Cheng <chengxianga2008@yahoo.com>
2015-12-11 21:01:00 +1100

    Jack update

--- wp-content/themes/balitawoo/index.php
+++ wp-content/themes/balitawoo/index.php
@@ -9,44 +9,42 @@
 			
 			<?php get_template_part( 'breadcrumbs' ); ?>
 			
-			<?php 
+			<?php if ( is_singular() ) { ?>
 			
-			$home_cat_obj = get_category_by_slug("home-post");
+				<?php hybrid_get_content_template(); // Loads the content template. ?>
 			
-			$query = new WP_Query( array( 'cat' => "-$home_cat_obj->term_id",  ) );
+			<?php
+				// Loads the comments.php template.
+				if ( comments_open() ) comments_template( '/comments.php', true ); 
+			?>
 			
-			if ( $query->have_posts() ) : ?>
-
-				<?php while ( $query->have_posts() ) : $query->the_post(); ?>
+			<?php }else{
 				
-					<?php hybrid_get_content_template(); // Loads the content template. ?>
-
-					<?php if ( is_singular() ) { ?>
-
-						<?php
-							// Loads the comments.php template.
-							if ( comments_open() ) comments_template( '/comments.php', true ); 
-						?>
-
-					<?php } ?>
-
-				<?php endwhile; ?>
-
-			<?php elseif ( current_user_can( 'edit_posts' ) ) : ?>
-
-				<?php get_template_part( 'no-results' ); ?>
-
-			<?php endif; ?>
-
-
-			<?php loop_pagination(); ?>
+				$home_cat_obj = get_category_by_slug("home-post");
+					
+				$query = new WP_Query( array( 'cat' => "-$home_cat_obj->term_id",  ) );
+					
+				if ( $query->have_posts() ) : ?>
+				
+					<?php while ( $query->have_posts() ) : $query->the_post(); ?>
+								
+						<?php hybrid_get_content_template(); // Loads the content template. ?>
+				
+					<?php endwhile; ?>
+				
+				<?php elseif ( current_user_can( 'edit_posts' ) ) : ?>
+				
+						<?php get_template_part( 'no-results' ); ?>
+				
+				<?php endif; ?>
+				
+				
+				<?php loop_pagination(); ?>
 			
-	
+			<?php }?>
 			
 		</div><!-- #content .site-content -->
 	</div>
-	
-	<div>
 	
 	<?php get_sidebar( 'primary' ); ?>
 	
